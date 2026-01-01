b7a9c7240ad1c47b58203026318424a8d712e279
flippette <me@flippo.cc>
2026-01-01 23:49:28 +0200

    fix overlay recursion 2

--- default.nix
+++ default.nix
@@ -6,34 +6,23 @@
       inherit system;
     },
 }: let
-  buildFfmpeg = {ffmpeg, ...}:
-    ffmpeg.override {
+  packages = pkgs: rec {
+    anienc =
+      pkgs.callPackage
+      ./nix/anienc.nix
+      {inherit ffmpeg;};
+
+    ffmpeg = pkgs.ffmpeg.override {
       svt-av1 = svt-av1-psyex;
     };
 
-  anienc =
-    pkgs.callPackage
-    ./nix/anienc.nix
-    {ffmpeg = buildFfmpeg pkgs;};
-
-  svt-av1-psyex =
-    pkgs.callPackage
-    ./nix/svt-av1-psyex.nix
-    {};
-in {
-  inherit
-    anienc
-    svt-av1-psyex
-    ;
-
-  ffmpeg = buildFfmpeg pkgs;
-
-  overlay = _: prev: {
-    inherit
-      anienc
-      svt-av1-psyex
-      ;
-
-    ffmpeg = buildFfmpeg prev;
+    svt-av1-psyex =
+      pkgs.callPackage
+      ./nix/svt-av1-psyex.nix
+      {};
   };
-}
+in
+  packages pkgs
+  // {
+    overlay = _: packages;
+  }
