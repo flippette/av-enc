567b04531e910716e1bb6192745cef35e9d8a4ad
flippette <me@flippo.cc>
2026-01-01 21:46:42 +0200

    simplify default.nix

--- default.nix
+++ default.nix
@@ -1,34 +1,29 @@
 {
   sources ? import ./npins,
   system ? builtins.currentSystem,
-  pkgs ? import sources.nixpkgs {inherit system;},
-}: let
-  inherit (pkgs) lib;
-in
-  lib.fix (self: {
-    packages = lib.fix (self: {
-      default = self.anienc;
-
-      anienc =
-        pkgs.callPackage
-        ./nix/anienc.nix
-        {
-          ffmpeg = pkgs.ffmpeg.override {
-            svt-av1 = self.svt-av1-psyex;
-          };
-        };
-
-      svt-av1-psyex =
-        pkgs.callPackage
-        ./nix/svt-av1-psyex.nix
-        {};
-    });
-
-    overlays.default = _: _: {
-      inherit
-        (self.packages)
-        anienc
-        svt-av1-psyex
-        ;
+  pkgs ?
+    import sources.nixpkgs {
+      inherit system;
+    },
+}: rec {
+  anienc =
+    pkgs.callPackage
+    ./nix/anienc.nix
+    {
+      ffmpeg = pkgs.ffmpeg.override {
+        svt-av1 = svt-av1-psyex;
+      };
     };
-  })
+
+  svt-av1-psyex =
+    pkgs.callPackage
+    ./nix/svt-av1-psyex.nix
+    {};
+
+  overlay = _: _: {
+    inherit
+      anienc
+      svt-av1-psyex
+      ;
+  };
+}
